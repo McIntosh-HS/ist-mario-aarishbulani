333667f79e3607050d086fddf33e8afdb16159c2
Aarish Bulani <2027bulani.aarish@fcboe.org>
2024-01-16 13:41:09 -0500

    ascii complete

---
+++ asciiart.c
@@ -0,0 +1,18 @@
+#Ascii art
+
+	
+
+	printf("               _                   \n");
+	printf("              | |                  \n");
+	printf("     _________|_|_                 \n");
+	printf("    /             \                \n");
+	printf("   /      ___      \               \n");
+	printf("  /      |   |      \              \n");
+	printf("  I      |___|      I              \n");
+	printf("  I                 I__________    \n");
+	printf("  I                 I  _____  I    \n");
+	printf("  I      ___        I |     | I    \n");
+	printf("  I     |   |       I |     | I    \n");
+	printf("  I     |   |       I |     | I    \n");
+
+
